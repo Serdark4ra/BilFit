40ed147fb1fb3558f5340d9c3a1b9193323c3ee0
Serdar Kara <serdar.kara@ug.bilkent.edu.tr>
2024-04-07 13:33:14 +0300

    google.json added

--- app/src/main/java/com/serdar_kara/bilfit/get_info_activities/BodyInfoActivity.java
+++ app/src/main/java/com/serdar_kara/bilfit/get_info_activities/BodyInfoActivity.java
@@ -1,5 +1,6 @@
 package com.serdar_kara.bilfit.get_info_activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,13 +10,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.serdar_kara.bilfit.R;
+import com.serdar_kara.bilfit.databinding.ActivityBodyInfoBinding;
 
 public class BodyInfoActivity extends AppCompatActivity {
-    
+    private ActivityBodyInfoBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityBodyInfoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent comingIntent = getIntent();
+        UserInfoHolder userInfoHolder = (UserInfoHolder) comingIntent.getSerializableExtra("userInfoHolder");
+
 
     }
 }
