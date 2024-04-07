6999c9dfea02ef5bd6a7b008bc82289f70f77238
Mehmet Akif Yavuz <yavuzmakif38@gmail.com>
2024-04-07 19:00:06 +0300

    "Friends" updated.
    Not anything functional by now.

--- app/src/main/java/com/serdar_kara/bilfit/FriendsActivity.java
+++ app/src/main/java/com/serdar_kara/bilfit/FriendsActivity.java
@@ -1,8 +1,11 @@
 package com.serdar_kara.bilfit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FriendsActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class FriendsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+
+        Toolbar toolbar = findViewById(R.id.toolbarFriend);
+        setSupportActionBar(toolbar);
+
+        // Add an onClickListener to the navigation icon
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FriendsActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
