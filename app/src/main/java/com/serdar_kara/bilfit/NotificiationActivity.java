39a7ae790dd426244a931b4641d8fe8ac76ae45b
Mehmet Akif Yavuz <yavuzmakif38@gmail.com>
2024-04-18 09:36:59 +0300

    "Notification" started

--- app/src/main/java/com/serdar_kara/bilfit/NotificiationActivity.java
+++ app/src/main/java/com/serdar_kara/bilfit/NotificiationActivity.java
@@ -1,26 +1,46 @@
 package com.serdar_kara.bilfit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.serdar_kara.bilfit.R;
 
 public class NotificiationActivity extends AppCompatActivity {
 
+    private com.serdar_kara.bilfit.databinding.ActivityMainBinding activityMainBinding;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private FirebaseFirestore db;
+    private DocumentReference documentReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_notificiation);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Toolbar toolbar = findViewById(R.id.toolbarNotification);
+        setSupportActionBar(toolbar);
+
+        // Add an onClickListener to the navigation icon
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NotificiationActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
