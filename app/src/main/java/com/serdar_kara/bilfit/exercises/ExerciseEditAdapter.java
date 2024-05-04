1cfcd15e02bc97c01eda11dd9f5bf252b256a479
Serdar Kara <serdar.kara@ug.bilkent.edu.tr>
2024-05-04 10:45:43 +0300

    Edit exercises is done

--- app/src/main/java/com/serdar_kara/bilfit/exercises/ExerciseEditAdapter.java
+++ app/src/main/java/com/serdar_kara/bilfit/exercises/ExerciseEditAdapter.java
@@ -1,30 +1,42 @@
 package com.serdar_kara.bilfit.exercises;
 
+import static android.content.ContentValues.TAG;
+
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.serdar_kara.bilfit.R;
 import com.serdar_kara.bilfit.algorithm.Tester;
 import com.serdar_kara.bilfit.databinding.ExerciseListActivityBaseModelBinding;
 import com.serdar_kara.bilfit.databinding.ExerciseListBaseModelBinding;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
 
 public class ExerciseEditAdapter extends RecyclerView.Adapter<ExerciseEditAdapter.ExerciseViewHolder>{
 
     private ArrayList<ExerciseModel> exerciseList;
     private Context context;
+    private String day;
 
-    public ExerciseEditAdapter(ArrayList<ExerciseModel> exerciseList, Context context) {
+    public ExerciseEditAdapter(ArrayList<ExerciseModel> exerciseList, Context context, String day) {
         this.exerciseList = exerciseList;
         this.context = context;
+        this.day = day;
     }
 
     @NonNull
@@ -49,15 +61,21 @@ public class ExerciseEditAdapter extends RecyclerView.Adapter<ExerciseEditAdapte
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle("Select an Exercise");
 
+        Tester tester = new Tester();
         // Example list of exercises. This should be dynamically loaded based on the muscle group or other criteria
-        String[] exercises = getExerciseList(position).toArray(new String[0]);
+        String[] exercises = Tester.returnAssociatedList(exerciseList.get(position).getName()).toArray(new String[0]);
 
         builder.setItems(exercises, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 // Update the exercise at the given position
-                exercises[position] = (exercises[which]);
+                String selectedExercise = exercises[which];
+                exerciseList.get(position).setName(selectedExercise);
                 notifyItemChanged(position);
+
+                // Update the exercise in the database
+                updateExerciseInArray(day, position, selectedExercise);
+
             }
         });
 
@@ -68,23 +86,62 @@ public class ExerciseEditAdapter extends RecyclerView.Adapter<ExerciseEditAdapte
         dialog.show();
     }
 
+
     private ArrayList<String> getExerciseList(int position) {
 
         ArrayList<String> exercises = new ArrayList<>();
-        exercises = Tester.returnBicepsExercises(exerciseList.get(position).getName());
-        if (exercises != null) {
-            return exercises;
-        }
 
-        exercises = Tester.returnBicepsExercises(exerciseList.get(position).getName());
-        if (exercises != null) {
-            return exercises;
-        }
         exercises.add("Exercise 1");
         exercises.add("Exercise 2");
         exercises.add("Exercise 3");
         return exercises;
     }
+
+    public void updateExerciseInArray(String day, int exerciseIndex, String newExerciseName) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
+
+        if (currentUser == null) {
+            Log.d(TAG, "No user logged in");
+            return; // Early return if the user is not logged in
+        }
+
+        DocumentReference docRef = FirebaseFirestore.getInstance()
+                .collection("Users")
+                .document(currentUser.getUid());
+
+        // Fetch the current array
+        docRef.get().addOnSuccessListener(documentSnapshot -> {
+            if (documentSnapshot.exists()) {
+                Map<String, Object> data = (Map<String, Object>) documentSnapshot.getData();
+                if (data != null && data.containsKey("program")) {
+                    Map<String, Object> program = (Map<String, Object>) data.get("program");
+                    if (program.containsKey(day)) {
+                        List<String> exercises = new ArrayList<>((List<String>) program.get(day));
+                        if (exerciseIndex >= 0 && exerciseIndex < exercises.size()) {
+                            exercises.set(exerciseIndex, newExerciseName);
+                            // Update the entire array for the day
+                            docRef.update("program." + day, exercises)
+                                    .addOnSuccessListener(aVoid -> Log.d(TAG, "Exercise updated successfully in " + day + " at index " + exerciseIndex))
+                                    .addOnFailureListener(e -> Log.w(TAG, "Error updating exercise at " + day + " index " + exerciseIndex, e));
+                        } else {
+                            Log.d(TAG, "Invalid exercise index");
+                        }
+                    } else {
+                        Log.d(TAG, day + " does not exist in the program");
+                    }
+                } else {
+                    Log.d(TAG, "Program data is missing in user document");
+                }
+            } else {
+                Log.d(TAG, "No user document exists for UID: " + currentUser.getUid());
+            }
+        }).addOnFailureListener(e -> Log.w(TAG, "Error retrieving user document", e));
+    }
+
+
+
+
 
 
 
