80124edbaede70c3867638c5cc18a328617a213d
Serdar Kara <serdar.kara@ug.bilkent.edu.tr>
2024-05-04 11:40:57 +0300

    Recreate program added

--- app/src/main/java/com/serdar_kara/bilfit/Settings/SettingsActivity.java
+++ app/src/main/java/com/serdar_kara/bilfit/Settings/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.serdar_kara.bilfit.Settings;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.serdar_kara.bilfit.databinding.ActivitySettingsBinding;
+import com.serdar_kara.bilfit.get_info_activities.GenderActivity;
 
 public class SettingsActivity extends AppCompatActivity {
     private ActivitySettingsBinding binding;
@@ -18,6 +21,21 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivitySettingsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.buttonChangePassword.setOnClickListener(v -> {
+            Intent intent = new Intent(SettingsActivity.this, ChangePasswordActivity.class);
+            startActivity(intent);
+        });
+
+        binding.buttonChangePhoto.setOnClickListener(v -> {
+            Intent intent = new Intent(SettingsActivity.this, ChangePhotoActivity.class);
+            startActivity(intent);
+        });
+
+        binding.buttonChangeProgram.setOnClickListener(v -> {
+            Intent intent = new Intent(SettingsActivity.this, GenderActivity.class);
+            startActivity(intent);
+        });
     }
 
 
