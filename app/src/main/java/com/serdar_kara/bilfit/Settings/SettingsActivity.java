e54f9719bff0d1f8fff7b4c4e8471f116808a180
Oğuzhan <ogzhndmr2200@gmail.com>
2024-05-09 11:44:57 +0300

    ayarlar ekranı ufak düzeltme

--- app/src/main/java/com/serdar_kara/bilfit/Settings/SettingsActivity.java
+++ app/src/main/java/com/serdar_kara/bilfit/Settings/SettingsActivity.java
@@ -3,6 +3,8 @@ package com.serdar_kara.bilfit.Settings;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextClock;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -11,22 +13,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.serdar_kara.bilfit.FeedbackActivity;
 import com.serdar_kara.bilfit.MainActivity;
+import com.serdar_kara.bilfit.R;
 import com.serdar_kara.bilfit.databinding.ActivitySettingsBinding;
 import com.serdar_kara.bilfit.get_info_activities.GenderActivity;
 import com.serdar_kara.bilfit.get_info_activities.UserInfoHolder;
 import com.serdar_kara.bilfit.login_activities.ForgotPasswordActivity;
 import com.serdar_kara.bilfit.login_activities.SignUpActivity;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 public class SettingsActivity extends AppCompatActivity {
     private ActivitySettingsBinding binding;
-
+    private TextView username;
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +43,11 @@ public class SettingsActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
+        username = findViewById(R.id.textView_user_name);
+        updateUsernameTextView();
+
+
+
 
         binding.buttonChangePassword.setOnClickListener(v -> {
             sendResetMail();
@@ -94,6 +106,28 @@ public class SettingsActivity extends AppCompatActivity {
         program.clear();
         userInfoHolder.regenerateWorkoutProgram();
     }
+    private void updateUsernameTextView() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("Users")
+                .document(currentUser.getUid()) // Geçerli kullanıcının UID'sini kullanarak belirli bir kullanıcının belgesini alın
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult(); // Belgeyi al
+
+                        if (document.exists()) { // Belge var mı diye kontrol et
+                            String userName = document.getString("name_surname");
+                            // TextView'a kullanıcı adını ayarla
+                            username.setText(userName);
+                        }
+                    }
+                });
+    }
+
+
+
+
 
 
 }
