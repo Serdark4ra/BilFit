bdc9760ac45e55c1b644db77371bac14981dc05e
Altan <altan.altay@ug.bilkent.edu.tr>
2024-05-10 23:25:03 +0300

    report sayfası kodu hazır
    ama biraz UI gelistirilir

--- app/src/main/java/com/serdar_kara/bilfit/ReportActivity.java
+++ app/src/main/java/com/serdar_kara/bilfit/ReportActivity.java
@@ -1,6 +1,45 @@
 package com.serdar_kara.bilfit;
 
+import static android.content.ContentValues.TAG;
+import static android.content.ContentValues.TAG;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.serdar_kara.bilfit.ProgramActivity.ProgramActivity;
+import com.serdar_kara.bilfit.Settings.SettingsActivity;
+import com.serdar_kara.bilfit.databinding.ActivityMainBinding;
+import com.serdar_kara.bilfit.exercises.ExerciseAdapter;
+import com.serdar_kara.bilfit.exercises.ExerciseModel;
+import com.serdar_kara.bilfit.friends.FriendsActivity;
+import com.serdar_kara.bilfit.get_info_activities.UserInfoHolder;
+import com.serdar_kara.bilfit.get_info_activities.UserInfoManager;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Map;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,19 +47,87 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.serdar_kara.bilfit.R;
+import com.serdar_kara.bilfit.databinding.ActivityMainBinding;
+import com.serdar_kara.bilfit.exercises.ExerciseAdapter;
+import com.serdar_kara.bilfit.exercises.ExerciseModel;
+
+import java.util.ArrayList;
+
 
 public class ReportActivity extends AppCompatActivity {
 
+    private ActivityMainBinding activityMainBinding;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+
+    private FirebaseFirestore db;
+    private DocumentReference documentReference;
+    private ArrayList<ExerciseModel> exerciseList;
+    private ExerciseAdapter exerciseAdapter;
+    TextView textView15;
+    TextView textView16;
+    TextView textView17;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_report);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+
+            textView15 = findViewById(R.id.textView15);
+            textView16 = findViewById(R.id.textView16);
+            textView17 = findViewById(R.id.textView17);
+
+            setTexts(currentUser.getUid());
+
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void setTexts(String userId) {
+        db = FirebaseFirestore.getInstance();
+        documentReference = db.collection("Users").document(userId);
+
+        documentReference.get().addOnSuccessListener(documentSnapshot -> {
+            if (documentSnapshot.exists()) {
+                // Retrieve and display name
+                System.out.println("MERHABA REPORT ACTIVITY");
+                // Retrieve and display points
+                Number pointsNumber = documentSnapshot.getLong("points");
+                if (pointsNumber != null) {  // Check if the points data exists
+                    int points = pointsNumber.intValue();
+
+                    double n1 = (double) points / 30000.0;
+                    double n2 = (double) points / 25000.0;
+
+                    String formattedN1 = String.format("%.6f", n1);
+                    String formattedN2 = String.format("%.6f", n2);
+
+                    textView15.setText("Calories have burned so far: " + points * 3);
+                    textView16.setText("You've reduced your risk of getting cancer by this much: %" + formattedN1);
+                    textView17.setText("You've reduced your risk of having a heart attack by this much: %" + formattedN2);
+
+                } else {
+                    textView15.setText("Calories have burned so far: " + 0);; // Default to 0 if no points
+                    textView16.setText("You've reduced your risk of getting cancer by this much: " + 0);
+                    textView17.setText("You've reduced your risk of having a heart attack by this much: " + 0);
+                }
+            } else {
+                Log.d("Error", "No such document with the current user id: " + userId);
+            }
+        }).addOnFailureListener(e -> Log.d(TAG, "Error fetching document", e));
     }
 }
