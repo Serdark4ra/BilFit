e5e471aff3b25a8f482e24eb239d3df1913e8337
Oğuzhan <ogzhndmr2200@gmail.com>
2024-05-12 13:44:24 +0300

    new report added
    kullanıcının bilfit kullanıcıları arasında puanına göre yüzde kaçlık dilimde olduğunu gösteriyor

--- app/src/main/java/com/serdar_kara/bilfit/ReportActivity.java
+++ app/src/main/java/com/serdar_kara/bilfit/ReportActivity.java
@@ -8,8 +8,19 @@ import androidx.appcompat.app.AppCompatActivity;
 /*import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;*/ //Burası bende hata veriyor
-
+import com.github.mikephil.charting.data.LineDataSet;*/
+import android.os.Bundle;
+import android.util.Log;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 import java.net.DatagramPacket;
 import java.util.ArrayList;
 import android.content.Context;
@@ -33,11 +44,15 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.serdar_kara.bilfit.ProgramActivity.ProgramActivity;
 import com.serdar_kara.bilfit.Settings.SettingsActivity;
+
 import com.serdar_kara.bilfit.databinding.ActivityMainBinding;
 import com.serdar_kara.bilfit.databinding.ActivityReportBinding;
 import com.serdar_kara.bilfit.databinding.ActivitySettingsBinding;
+
 import com.serdar_kara.bilfit.exercises.ExerciseAdapter;
 import com.serdar_kara.bilfit.exercises.ExerciseModel;
 import com.serdar_kara.bilfit.friends.FriendsActivity;
@@ -75,13 +90,13 @@ public class ReportActivity extends AppCompatActivity {
     private ActivityMainBinding activityMainBinding;
     private FirebaseAuth mAuth;
     private FirebaseUser currentUser;
-
+    private int thisWeekPoints = 0;
+    private int lastWeekPoints = 0;
     private FirebaseFirestore db;
     private DocumentReference documentReference;
     private ArrayList<ExerciseModel> exerciseList;
     private int goalpoints = 10000;
-    private int index = 0;
-    private int day = 0;
+
 
     private ActivityReportBinding binding;
 
@@ -89,6 +104,8 @@ public class ReportActivity extends AppCompatActivity {
     TextView caloriesText;
     TextView cancerText;
     TextView heartAttackText;
+
+    TextView daysToGoalText;
 
 
     @Override
@@ -111,10 +128,12 @@ public class ReportActivity extends AppCompatActivity {
         setContentView(R.layout.activity_report);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 
-
             caloriesText = findViewById(R.id.caloriesText);
             cancerText = findViewById(R.id.cancerText);
             heartAttackText = findViewById(R.id.heartAttackText);
+            daysToGoalText = findViewById(R.id.textView15);
+
+
 
             setTexts(currentUser.getUid());
 
@@ -127,7 +146,6 @@ public class ReportActivity extends AppCompatActivity {
     private void setTexts(String userId) {
         db = FirebaseFirestore.getInstance();
         documentReference = db.collection("Users").document(userId);
-
         documentReference.get().addOnSuccessListener(documentSnapshot -> {
             if (documentSnapshot.exists()) {
                 // Retrieve and display name
@@ -136,13 +154,9 @@ public class ReportActivity extends AppCompatActivity {
                 Number pointsNumber = documentSnapshot.getLong("points");
                 if (pointsNumber != null) {  // Check if the points data exists
                     int points = pointsNumber.intValue();
-                    day = calculateAverageDailyPoints(points , index);
-                    showDaysToReachGoal(points , goalpoints , day);
-
-
-
                     double n1 = (double) points / 30000.0;
                     double n2 = (double) points / 25000.0;
+                    findCurrentUserPlace();
 
                     String formattedN1 = String.format("%.6f", n1);
                     String formattedN2 = String.format("%.6f", n2);
@@ -161,39 +175,33 @@ public class ReportActivity extends AppCompatActivity {
             }
         }).addOnFailureListener(e -> Log.d(TAG, "Error fetching document", e));
     }
-    private int calculateAverageDailyPoints(int totalPoints, int daysPassed) {
-        // Eğer hiç gün geçmediyse, ortalama günlük puan 0 olmalı
-        if (daysPassed == 0) {
-            index++;
-            return 1;
-
-        }
-
-        // Ortalama günlük puanı hesaplayın
-        int averageDailyPoints = totalPoints / daysPassed;
-        index++;
-
-        return averageDailyPoints;
-    }
-    private void showDaysToReachGoal(int totalPoints, int goalPoints, int averageDailyPoints) {
-        // Hedefe ulaşmak için kalan gün sayısını hesaplayın
-        int remainingDays;
-        if (averageDailyPoints != 0 ) { // divide by 0 error here
-            remainingDays = (goalPoints - totalPoints) / averageDailyPoints;
-        } else {
-            remainingDays = (goalPoints - totalPoints) / 100;
-        }
-
-        // Hedefe ulaşmak için kalan gün sayısını göstermek için bir TextView bulun
-        TextView daysToGoalText = findViewById(R.id.textView15);
-
-        // Eğer kalan gün sayısı negatifse, kullanıcı zaten hedefine ulaşmıştır veya hedefe ulaşması imkansızdır
-        if (remainingDays < 0) {
-            daysToGoalText.setText("You've already reached your goal!\nYou've new goal.");
-            goalpoints = 2 * goalpoints;
-
-        } else {
-            daysToGoalText.setText("Days left to reach your goal: " + remainingDays);
+    private void findCurrentUserPlace() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+            db.collection("Users")
+                    .orderBy("points", Query.Direction.DESCENDING)
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            List<DocumentSnapshot> documents = task.getResult().getDocuments();
+                            int currentUserRank = 0;
+                            int totalUsers = documents.size();
+                            for (int i = 0; i < documents.size(); i++) {
+                                if (documents.get(i).getId().equals(userId)) {
+                                    currentUserRank = i + 1; // Rank starts from 1
+                                    break;
+                                }
+                            }
+                            double percentage = 100.0 - ((double) currentUserRank / totalUsers * 100);
+                            String message = "Congratulations! You have exercised more than " + String.format(Locale.getDefault(), "%.2f", percentage) + "% of Bilfit users.";
+                            // Şimdi bu mesajı bir TextView'e yazdıralım. Örneğin:
+                            daysToGoalText.setText(message);
+                        } else {
+                            Log.e(TAG, "Error getting documents: ", task.getException());
+                        }
+                    });
         }
     }
 
